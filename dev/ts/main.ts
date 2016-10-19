7bbc2c3294852ffd2b2639fff0e2c2f66b735418
yukiIchige <info@yuki-web.net>
2016-10-19 20:36:37 +0900

    get,setの練習 refs #10

--- dev/ts/main.ts
+++ dev/ts/main.ts
@@ -6,21 +6,31 @@ $(function() {
 
 import { ToggleMenu } from "./toggleMenu";
 
-var options = {
-    nav: ".nav",
-    btnOpen: ".open-menu",
-    btnClose: ".close-menu",
-};
-
 $(function() {
+    let options = {
+        nav: ".nav",
+        btnOpen: ".open-menu",
+        btnClose: ".close-menu",
+    };
+
     new ToggleMenu(options);
 });
 
 import { Tab } from "./tab";
 
 $(function() {
-    new Tab({
+    let options = {
         tabs: ".tab-menu li",
         contents: ".tab-contents",
-    });
+    };
+    new Tab(options);
+});
+
+import { Name } from "./getset";
+
+$(function() {
+    var yuki = new Name();
+    yuki.setName("yuki");
+    var name = yuki.getName();
+    // console.log(name);
 });
--- dev/ts/tab.ts
+++ dev/ts/tab.ts
@@ -1,4 +1,5 @@
 "use strict";
+
 interface Options {
     tabs: string;
     contents: string;
